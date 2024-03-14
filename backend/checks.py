13eeb72b316a9fdfd1b6c75542d580dc67e6642f
salopietari <pietari.salo1@icloud.com>
2024-03-14 11:13:21 +0200

    remove old (now unused) checks

--- backend/checks.py
+++ backend/checks.py
@@ -5,58 +5,6 @@ from backend.exceptions import *
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from backend.loghandler import *
 from django.contrib.auth.hashers import check_password
-
-# used in registration
-@csrf_exempt
-def check_registration(data):
-    try:
-        required_fields = ['username', 'password', 'confirmPassword', 'unit', 'experience', 'email']
-        for field in required_fields:
-            if not data.get(field): # check that no required field is empty
-                raise Exception(f"{field} is required")
-            
-        if len(data['password']) < 5:
-            raise PasswordTooShortError("Password must be at least 5 characters long")
-        
-        if (data['password'] != data['confirmPassword']):
-            raise PasswordsDoNotMatchError("Passwords do not match")
-        
-        # already exists
-        if CustomUser.objects.filter(username=data.get("username")).exists():
-            raise Exception("Username already exists")
-
-        if CustomUser.objects.filter(email=data.get("email")).exists():
-            raise Exception("Email already exists")
-        
-    except PasswordTooShortError as e:
-        raise PasswordTooShortError(e)
-    
-    except PasswordsDoNotMatchError as e:
-        raise PasswordsDoNotMatchError(e)
-    
-    except Exception as e: 
-        raise Exception(e)
-    
-# used when user is logging in
-@csrf_exempt
-def check_login(data):
-    try:
-        username = data.get("username")
-        password = data.get("password")
-
-        # check if empty
-        if not username:
-            raise Exception("Username is required")
-        if not password:
-            raise Exception("Password is required")
-
-        user = CustomUser.objects.get(username=username)
-        
-        if check_password(password, user.password) is not True:
-            raise Exception("Wrong password")
-    
-    except Exception as e:
-        raise Exception(e)
     
 @csrf_exempt
 def check_username(username):
