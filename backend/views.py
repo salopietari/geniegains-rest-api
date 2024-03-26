989eed4737aea7a80ad3b15a2eff10480f1bddb5
salopietari <122457202+salopietari@users.noreply.github.com>
2024-03-26 22:23:04 +0200

    switch openai model

--- backend/views.py
+++ backend/views.py
@@ -923,7 +923,7 @@ class question(APIView):
             data = json.loads(request.body)
             question = data.get("question")
             response = client.chat.completions.create(
-                model="gpt-3.5-turbo",
+                model="gpt-3.5-turbo-0125",
                 messages=[
                     {"role": "system", "content": "You are a helpful gym personl trainer, your job is to answer any questions the user might have."},
                     {"role": "user", "content": f"{question}"},
